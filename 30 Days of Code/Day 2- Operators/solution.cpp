2f1bcf8005e34202c56ef43ee595d82ede4f0a4b
Donato Rimenti <donatohan.rimenti@gmail.com>
2018-03-02 12:22:14 +0100

    Create solution.cpp

---
+++ 30 Days of Code/Day 2- Operators/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double meal_cost;
+    cin >> meal_cost;
+    int tip_percent;
+    cin >> tip_percent;
+    int tax_percent;
+    cin >> tax_percent;
+    
+    // Computes the total cost.
+    double tip = meal_cost * tip_percent / 100;
+    double tax = meal_cost * tax_percent / 100;
+    int total_cost = meal_cost + tip + tax;
+    
+    // Prints the result.
+    cout << "The total meal cost is " << total_cost << " dollars." << endl;
+    
+    return 0;
+}
