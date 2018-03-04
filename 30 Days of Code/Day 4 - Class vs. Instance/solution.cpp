2b0ee2736310cd1a460105a9daf34b513c1a1def
Donato Rimenti <donatohan.rimenti@gmail.com>
2018-03-04 21:26:58 +0100

    Fixed formatting

--- 30 Days of Code/Day 4 - Class vs. Instance/solution.cpp
+++ 30 Days of Code/Day 4 - Class vs. Instance/solution.cpp
@@ -37,18 +37,18 @@ class Person{
     
 int main(){
     int t;
-	  int age;
+    int age;
     cin >> t;
     for(int i=0; i < t; i++) {
     	cin >> age;
         Person p(age);
         p.amIOld();
         for(int j=0; j < 3; j++) {
-        	p.yearPasses(); 
+            p.yearPasses(); 
         }
         p.amIOld();
       
-		cout << '\n';
+	cout << '\n';
     }
 
     return 0;
