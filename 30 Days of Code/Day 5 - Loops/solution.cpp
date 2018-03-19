73c61eff831acb7b9d3199056514c63fb0b14ef3
Donato Rimenti <donatohan.rimenti@gmail.com>
2018-03-19 10:00:52 +0100

    Create solution.cpp

---
+++ 30 Days of Code/Day 5 - Loops/solution.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < 10; i++) {
+        cout << n << " x " << i + 1 << " = " << n * (i + 1) << endl;
+    }
+    
+    return 0;
+}
