c0bb68a4c8328960c38f8ef53f82759329e8c423
Donato Rimenti <donatohan.rimenti@gmail.com>
2018-03-29 16:15:33 +0200

    Create solution.cc

---
+++ 30 Days of Code/Day 27 - Testing/solution.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "5" << endl;
+    
+    // Test case 1.
+    cout << "4 3" << endl;
+    cout << "-1 0 4 2" << endl;
+    
+    // Test case 2.
+    cout << "5 2" << endl;
+    cout << "0 -1 2 1 4" << endl;
+    
+    // Test case 3.
+    cout << "7 6" << endl;
+    cout << "2 0 -1 1 1 1 1" << endl;
+    
+    // Test case 4.
+    cout << "3 1" << endl;
+    cout << "-1 0 4" << endl;
+    
+    // Test case 5.
+    cout << "6 4" << endl;
+    cout << "0 -1 1 4 5 6" << endl;
+    
+}
